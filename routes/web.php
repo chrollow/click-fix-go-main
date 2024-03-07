8b9eca9daf77a6bd35328a3346f6a4ecc08ad1cd
Jlon718 <johnleonard.nagallo@tup.edu.ph>
2024-03-07 10:25:44 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 
 //hello
+//hello
 /*
 |--------------------------------------------------------------------------
 | Web Routes
